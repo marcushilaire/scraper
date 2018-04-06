8169c08f4e6e98e1dc1c352718fc205a1c157d83
Marucs Hilaire <marcushilaire@The-Mac.fios-router.home>
2018-04-06 00:36:45 -0400

    if it was a missing semi colon i will cry

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 var db = require("./models");
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
